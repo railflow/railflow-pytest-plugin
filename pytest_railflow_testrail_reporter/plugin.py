90a11b8e33b2ba26118d63e3b357883b9e0629ab
Athul R T <athul@disroot.org>
2021-08-16 14:55:11 +0530

    added workflow

--- pytest_railflow_testrail_reporter/plugin.py
+++ pytest_railflow_testrail_reporter/plugin.py
@@ -7,12 +7,12 @@ import pytest
 from _pytest.mark.structures import Mark
 
 
-
-
 _py_ext_re = re.compile(r"\.py$")
 
+
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
-    return '%s:%s: %s:%s\n' % (filename, lineno, category.__name__, message)
+    return "%s:%s: %s:%s\n" % (filename, lineno, category.__name__, message)
+
 
 warnings.formatwarning = warning_on_one_line
 
@@ -22,11 +22,13 @@ def pytest_addoption(parser):
     Adds commandline option for creating the json file.
     """
     group = parser.getgroup("Json report")
-    group.addoption("--jsonfile",
-                    action="store",
-                    dest="jsonpath",
-                    default=None,
-                    help='name of the json file where test details are saved.')
+    group.addoption(
+        "--jsonfile",
+        action="store",
+        dest="jsonpath",
+        default=None,
+        help="name of the json file where test details are saved.",
+    )
 
 
 def pytest_configure(config):
@@ -38,13 +40,11 @@ def pytest_configure(config):
         config.json = JiraJsonReport(jsonpath)
         config.pluginmanager.register(config.json)
     # register an additional marker
-    config.addinivalue_line(
-        "markers", "railflow(options): read custom metadata"
-    )
+    config.addinivalue_line("markers", "railflow(options): read custom metadata")
 
 
 def pytest_unconfigure(config):
-    json = getattr(config, 'json', None)
+    json = getattr(config, "json", None)
     if json:
         del config.json
         config.pluginmanager.unregister(json)
@@ -52,24 +52,27 @@ def pytest_unconfigure(config):
 
 def mangle_test_address(address):
     """Split and modify test address to required format"""
-    path, brack, params = address.partition('[')
+    path, brack, params = address.partition("[")
     names = path.split("::")
     try:
-        names.remove('()')
+        names.remove("()")
     except ValueError:
         pass
 
-    names[0] = names[0].replace("/", '.')
+    names[0] = names[0].replace("/", ".")
     names[0] = _py_ext_re.sub("", names[0])
     names[-1] += brack + params
     return names
-    
+
+
 def restructure(data):
     restructured_list = []
     temp_list = []
     for i in data:
         if isinstance(i, OrderedDict):
-            restructured_dict = OrderedDict([('railflow_test_attributes', OrderedDict(temp_list))])
+            restructured_dict = OrderedDict(
+                [("railflow_test_attributes", OrderedDict(temp_list))]
+            )
             restructured_dict.update(i)
             restructured_list.append(restructured_dict)
             temp_list = []
@@ -86,16 +89,30 @@ class JiraJsonReport(object):
     def __init__(self, jsonpath):
         self.results = []
         self.jsonpath = jsonpath
-        self.class_list = ['testrail_user', 'testrail_project', 'case_fields',
-                            'result_fields', 'test_path', 'case_type',
-                            'case_priority','assign']
-        self.fun_list = ['author', 'description', 'jira_id', 'test_path',
-                        'case_fields', 'result_fields', 'id_mappings',
-                        'case_type', 'case_priority']
+        self.class_list = [
+            "testrail_user",
+            "testrail_project",
+            "case_fields",
+            "result_fields",
+            "test_path",
+            "case_type",
+            "case_priority",
+            "assign",
+        ]
+        self.fun_list = [
+            "author",
+            "description",
+            "jira_id",
+            "test_path",
+            "case_fields",
+            "result_fields",
+            "id_mappings",
+            "case_type",
+            "case_priority",
+        ]
 
     def append(self, result):
         self.results.append(result)
-
 
     def build_result(self, report, status, message):
         """
@@ -104,26 +121,24 @@ class JiraJsonReport(object):
         result = OrderedDict()
         names = mangle_test_address(report.nodeid)
 
-        result['suite_name'] = names[-2]
-        result['test_name'] = names[-1]
+        result["suite_name"] = names[-2]
+        result["test_name"] = names[-1]
         if report.test_doc is None:
-            result['details'] = report.test_doc
+            result["details"] = report.test_doc
         else:
-            result['details'] = report.test_doc.strip()
-        result['markers'] = report.test_marker
-        result['result'] = status
-        result['duration'] = getattr(report, 'duration', 0.0)
-        result['timestamp'] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-        result['message'] = message
-        result['file_name'] = report.location[0]
+            result["details"] = report.test_doc.strip()
+        result["markers"] = report.test_marker
+        result["result"] = status
+        result["duration"] = getattr(report, "duration", 0.0)
+        result["timestamp"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        result["message"] = message
+        result["file_name"] = report.location[0]
         self.append(result)
-
 
     def append_pass(self, report):
         status = "PASSED"
         message = None
         self.build_result(report, status, message)
-
 
     def append_failure(self, report):
 
@@ -133,24 +148,22 @@ class JiraJsonReport(object):
 
         else:
             message = str(report.longrepr)
-            #if hasattr(report.longrepr, "reprcrash"):
-                #message = report.longrepr.reprcrash.message
-            #elif isinstance(report.longrepr, (unicode, str)):
-                #message = report.longrepr
-            #else:
-                #message = str(report.longrepr)
+            # if hasattr(report.longrepr, "reprcrash"):
+            # message = report.longrepr.reprcrash.message
+            # elif isinstance(report.longrepr, (unicode, str)):
+            # message = report.longrepr
+            # else:
+            # message = str(report.longrepr)
 
             status = "FAILED"
 
         self.build_result(report, status, message)
-
 
     def append_error(self, report):
 
         message = report.longrepr
         status = "ERROR"
         self.build_result(report, status, message)
-
 
     def append_skipped(self, report):
 
@@ -166,8 +179,6 @@ class JiraJsonReport(object):
 
         self.build_result(report, status, message)
 
-
-
     @pytest.mark.hookwrapper
     def pytest_runtest_makereport(self, item, call):
 
@@ -177,30 +188,32 @@ class JiraJsonReport(object):
         report.test_doc = item.obj.__doc__
         test_marker = []
         for k, v in item.keywords.items():
-            if isinstance(v,list):
+            if isinstance(v, list):
                 for x in v:
-                    if isinstance(x,Mark) and x.name != 'railflow':
+                    if isinstance(x, Mark) and x.name != "railflow":
                         test_marker.append(x.name)
-                    
-        report.test_marker = ', '.join(test_marker)
 
-        if report.when == "call":                
+        report.test_marker = ", ".join(test_marker)
+
+        if report.when == "call":
             for mark in item.iter_markers(name="railflow"):
                 for i in mark.kwargs:
                     if item.cls:
                         if i in self.fun_list:
-                                self.results.append((i,mark.kwargs[i]))
+                            self.results.append((i, mark.kwargs[i]))
                         elif i in self.class_list:
-                            self.results.append((i,mark.kwargs[i]))
+                            self.results.append((i, mark.kwargs[i]))
                         else:
-                            warnings.warn("%s is not a valid class attribute" % i, UserWarning)
+                            warnings.warn(
+                                "%s is not a valid class attribute" % i, UserWarning
+                            )
                     else:
                         if i in self.fun_list:
-                            self.results.append((i,mark.kwargs[i]))
+                            self.results.append((i, mark.kwargs[i]))
                         else:
-                            warnings.warn("%s is not a valid test attribute" % i, UserWarning)
-                            
-                    
+                            warnings.warn(
+                                "%s is not a valid test attribute" % i, UserWarning
+                            )
 
     def pytest_runtest_logreport(self, report):
 
@@ -218,20 +231,25 @@ class JiraJsonReport(object):
         elif report.skipped:
             self.append_skipped(report)
 
-
     def pytest_sessionfinish(self, session):
-        if not hasattr(session.config, 'slaveinput'):
+        if not hasattr(session.config, "slaveinput"):
             if self.results:
                 fieldnames = restructure(self.results)
                 if self.jsonpath:
                     filepath = self.jsonpath
-                    with open(filepath, 'w') as file:
-                        json.dump(fieldnames, file, sort_keys=False, indent=4, separators=(',', ': '))
-           
-            
+                    with open(filepath, "w") as file:
+                        json.dump(
+                            fieldnames,
+                            file,
+                            sort_keys=False,
+                            indent=4,
+                            separators=(",", ": "),
+                        )
 
     def pytest_terminal_summary(self, terminalreporter):
         if self.results:
-            terminalreporter.write_sep("-*","Json report written to %s" % self.jsonpath)
+            terminalreporter.write_sep(
+                "-*", "Json report written to %s" % self.jsonpath
+            )
         else:
-            terminalreporter.write_sep("-*","No Json report is created.")
+            terminalreporter.write_sep("-*", "No Json report is created.")
--- tests/test_railflow.py
+++ tests/test_railflow.py
@@ -1,21 +1,22 @@
-import pytest
 import json
+import pytest
 
 
 @pytest.fixture()
 def sample_test(testdir):
-    testdir.makepyfile("""
-    import pytest 
-    
+    testdir.makepyfile(
+        """
+    import pytest
+
     @pytest.mark.railflow(author='Bob',  description='Addition of two numbers',
-                    jira_id=100231, test_path='test_calculation.py', 
+                    jira_id=100231, test_path='test_calculation.py',
                     case_fields='filedA1', result_fields='fieldB1',
                     id_mappings='map id1', case_type='test case', case_priority='important')
     def test_pass():
         assert 1==1
-        
-    @pytest.mark.railflow(testrail_user='Nulli', 
-                    testrail_project= "Mathematics", 
+
+    @pytest.mark.railflow(testrail_user='Nulli',
+                    testrail_project= "Mathematics",
                     case_fields='field',
                     result_fields='output',
                     test_path='manipulation.py',
@@ -23,23 +24,21 @@ def sample_test(testdir):
                     case_priority='Important',
                     assign=['user1@gmail.com', 'user2@gmail.com'])
     class TestClass:
-    
+
         def test_fail(self):
             assert 4 == 5
-        
-    
-    """)
-    return testdir
-    
+        """
+        )
+
+
 @pytest.fixture()
 def load_json(sample_test):
-    result = sample_test.runpytest('--jsonfile=output.json')
-    
-    with open('output.json','r') as f:
+    sample_test.runpytest("--jsonfile=output.json")
+
+    with open("output.json", "r") as f:
         report = json.load(f)
-    
+
     return report
-    
 
 
 def test_open_json(load_json):
@@ -48,54 +47,86 @@ def test_open_json(load_json):
     """
     if type(load_json[0]) == dict:
         assert True
-        
-        
-@pytest.mark.parametrize("A,B",[("author",'Bob'),('description','Addition of two numbers'),
-                        ("jira_id", 100231),('test_path','test_calculation.py'),('case_fields','filedA1'),
-                        ('result_fields','fieldB1'),('id_mappings', 'map id1'),('case_type','test case'),
-                        ('case_priority','important')])  
-def test_json(load_json,A,B):
+
+
+@pytest.mark.parametrize(
+    "A, B",
+    [
+        ("author", "Bob"),
+        ("description", "Addition of two numbers"),
+        ("jira_id", 100231),
+        ("test_path", "test_calculation.py"),
+        ("case_fields", "filedA1"),
+        ("result_fields", "fieldB1"),
+        ("id_mappings", "map id1"),
+        ("case_type", "test case"),
+        ("case_priority", "important"),
+    ],
+)
+def test_json(load_json, A, B):
     """
     Tests if railflow_test_attributes are correctly printed in json report
     """
-    test_attr = load_json[0]['railflow_test_attributes']
+    test_attr = load_json[0]["railflow_test_attributes"]
     assert test_attr[A] == B
-    
 
 
-@pytest.mark.parametrize("A,B",[('suite_name','test_json_test_report'), ('test_name','test_pass'), ('details', None), ('markers', ''),
-                        ('result', 'PASSED'),('file_name', 'test_json_test_report.py')])    
-def test_json_test_report(load_json,A, B):
+@pytest.mark.parametrize(
+    "A,B",
+    [
+        ("suite_name", "test_json_test_report"),
+        ("test_name", "test_pass"),
+        ("details", None),
+        ("markers", ""),
+        ("result", "PASSED"),
+        ("file_name", "test_json_test_report.py"),
+    ],
+)
+def test_json_test_report(load_json, A, B):
     """
     Tests report paramters
     """
 
     report_dict = load_json[0]
     assert report_dict[A] == B
-    
- 
 
-@pytest.mark.parametrize("A,B",[("testrail_user",'Nulli'),('testrail_project','Mathematics'),
-                        ('test_path','manipulation.py'),('case_fields','field'),
-                        ('result_fields','output'),('case_type','Normal tests'),
-                        ('case_priority','Important'), ('assign', ['user1@gmail.com', 'user2@gmail.com'])])  
-def test_json_class(load_json,A,B):
+
+@pytest.mark.parametrize(
+    "A,B",
+    [
+        ("testrail_user", "Nulli"),
+        ("testrail_project", "Mathematics"),
+        ("test_path", "manipulation.py"),
+        ("case_fields", "field"),
+        ("result_fields", "output"),
+        ("case_type", "Normal tests"),
+        ("case_priority", "Important"),
+        ("assign", ["user1@gmail.com", "user2@gmail.com"]),
+    ],
+)
+def test_json_class(load_json, A, B):
+
     """
     Tests railflow_test_attributes in class.
     """
-    test_attr = load_json[1]['railflow_test_attributes']
+
+    test_attr = load_json[1]["railflow_test_attributes"]
     assert test_attr[A] == B
-    
 
 
-
-@pytest.mark.parametrize("A,B",[('suite_name','TestClass'), ('test_name','test_fail'), ('details', None),
-                        ('result', 'FAILED'),('file_name', 'test_json_class_report.py')])    
-def test_json_class_report(load_json,A, B):
+@pytest.mark.parametrize(
+    "A,B",
+    [
+        ("suite_name", "TestClass"),
+        ("test_name", "test_fail"),
+        ("details", None),
+        ("result", "FAILED"),
+        ("file_name", "test_json_class_report.py"),
+    ],
+)
+def test_json_class_report(load_json, A, B):
     """
     Tests failed class report parameters
     """
     report_dict = load_json[1]
     assert report_dict[A] == B
-  
-
